0d8fb7734ea5387c48124f44afc903e22abb6a7e
Julio Azancort <julioazancortneto@gmail.com>
2021-05-11 11:25:50 -0300

    Adição de imagens no dashboard

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,9 +1,44 @@
 <template>
-  <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-full.svg"
-    >
+  <q-page class="">
+    <div class="row q-pt-xl">
+      <img
+        class="col"
+        alt="App image"
+        src="~assets/img/app.webp"
+        width="20"
+      >
+      <img
+        class="col"
+        alt="Mercado image"
+        src="~assets/img/mercado.webp"
+        width="20"
+      >
+      <img
+        class="col"
+        alt="Ofertas image"
+        src="~assets/img/ofertas.webp"
+        width="20"
+      >
+    </div>
+    <div class="row q-pt-xs">
+      <img
+        class="col"
+        alt="Ofertas de 99 image"
+        src="~assets/img/oferta99.webp"
+        width="20"
+      >
+      <img
+        class="col"
+        alt="SmartTv image"
+        src="~assets/img/smartTV.webp"
+        width="20"
+      >
+      <img
+        class="col"
+        alt="Celulares image"
+        src="~assets/img/celular.webp"
+      >
+    </div>
   </q-page>
 </template>
 
